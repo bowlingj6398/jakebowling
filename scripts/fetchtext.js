4e7bec1f9f25ba853b2211f56296dbf7dae13648
bowlingj6398 <hydroflame521.busines@gmail.com>
2024-08-26 23:58:10 -0500

    Update fetchtext.js

--- scripts/fetchtext.js
+++ scripts/fetchtext.js
@@ -1,11 +1,17 @@
+// wait for page to load
 document.addEventListener('DOMContentLoaded', function() {
+    //define the html container
     const textContainer = document.getElementById('text-container');
+    //determine which text file should be fetched based on html argument
     const fileToLoad = textContainer.getAttribute('data-file');
 
+    //grab the text file and make it the text inside the container
     fetch(fileToLoad)
         .then(response => response.text())
         .then(data => {
             textContainer.innerText = data;
         })
+
+        //if it doesn't work then it'll print a reason in the developer console
         .catch(error => console.error('unable to fetch:', error));
 });
