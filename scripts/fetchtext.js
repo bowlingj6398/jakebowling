ed876cbfedc0b03da680306dc5314563b63e85de
bowlingj6398 <hydroflame521.busines@gmail.com>
2024-10-17 12:12:34 -0500

    Update fetchtext.js

--- scripts/fetchtext.js
+++ scripts/fetchtext.js
@@ -2,6 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     //define the html container
     const textContainer = document.getElementById('text-container');
+    const textContainer1 = document.getElementById('text-container1');
+    const textContainer2 = document.getElementById('text-container2');
     //determine which text file should be fetched based on html argument
     const fileToLoad = textContainer.getAttribute('data-file');
 
@@ -10,6 +12,8 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.text())
         .then(data => {
             textContainer.innerText = data;
+            textContainer1.innerText = data;
+            textContainer2.innerText = data;
         })
 
         //if it doesn't work then it'll print a reason in the developer console
